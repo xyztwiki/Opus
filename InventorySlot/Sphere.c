fa89462b775ec4b3fa7a63f89e7ca26a11b31c5a
threeOS <blackfairyforma726@gmail.com>
2024-12-07 11:29:55 +0800

    Update Sphere.c

--- InventorySlot/Sphere.c
+++ InventorySlot/Sphere.c
@@ -1,15 +1,15 @@
-#include "InventorySlot84.ts"
-#include "InventorySlot369.ts"
-#include "InventorySlot1234.ts"
-#include "InventorySlot567890.ts"
-#include "N0InventorySlot.ts"
-#include "N1InventorySlot.ts"
+#include "Slot84.ts"
+#include "Slot369.ts"
+#include "Inventory1234.ts"
+#include "Inventory567890.ts"
+#include "N0Inventory.ts"
+#include "N1Inventory.ts"
 #include "logInventorySlot.ts"
 #include "XYZ.ts"
 void InventortSlot(){
-    int InventorySlot2017 = "InventorySlot84 + InventorySlot369";
-    int InventorySlot520 = "InventorySlot1234 + InventorySlot567890";
-    int NInventorySlot = "N1InventorySlot + N0InventorySlot";
-    int logInventorySlot = "InventorySlot2017 + InventorySlot520 + NInventorySlot";
+    int Slot = "Slot84 + Slot369";
+    int Inventory = "Inventory1234 + Inventory567890";
+    int NInventory = "N1Inventory + N0Inventory";
+    int logInventorySlot = "Slot + Inventory + NInventory";
 }
 
