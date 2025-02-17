776116748eb7712b6d7c60a35bc46a0013d9a592
xyztwiki <blackfairyforma726@gmail.com>
2025-02-17 12:54:51 +0800

    Update fermat0.ts

--- tensor/fermat/fermat0.ts
+++ tensor/fermat/fermat0.ts
@@ -2,6 +2,6 @@ import './tensor/tensor19'
 import './tensor/tensor91'
 import './tensor/zerotensor'
 void function fermat0(){
- void ['tensor19'+'tensor91'=== 'zerotensor']
+ void ['tensor19'+'tensor91'== 'zerotensor']
   return fermat0
 }
